3d046c0ab809bc84d90e43311b0fe38665080f6d
Maciej Kedra <maciejken@gmail.com>
2023-09-21 15:46:26 +0200

    update .env

--- src/lib/firebase/index.ts
+++ src/lib/firebase/index.ts
@@ -5,16 +5,12 @@ import {
   FIREBASE_STORAGE_BUCKET,
   FIREBASE_MESSAGING_SENDER_ID,
   FIREBASE_APP_ID,
-} from '@env';
+} from "@env";
 
 import { initializeApp } from "firebase/app";
 // import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: FIREBASE_API_KEY,
   authDomain: FIREBASE_AUTH_DOMAIN,
@@ -28,4 +24,3 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
-// const analytics = getAnalytics(app);
--- src/types/env.d.ts
+++ src/types/env.d.ts
@@ -1,14 +1,10 @@
-declare module '@env' {
-    // Firebase config (see .env -> Secrets)
-    export const FIREBASE_API_KEY: string;
-    export const FIREBASE_AUTH_DOMAIN: string;
-    export const FIREBASE_PROJECT_ID: string;
-    export const FIREBASE_STORAGE_BUCKET: string;
-    export const FIREBASE_MESSAGING_SENDER_ID: string;
-    export const FIREBASE_APP_ID: string;
-    export const FIREBASE_MEASUREMENT_ID: string;
-
-    // other/public variables
-    export const EXPO_PUBLIC_HEADER_TITLE: string;
-    export const EXPO_PUBLIC_INITIAL_BEANS_ID: string;
+declare module "@env" {
+  // Firebase config (see .env -> Secrets)
+  export const FIREBASE_API_KEY: string;
+  export const FIREBASE_AUTH_DOMAIN: string;
+  export const FIREBASE_PROJECT_ID: string;
+  export const FIREBASE_STORAGE_BUCKET: string;
+  export const FIREBASE_MESSAGING_SENDER_ID: string;
+  export const FIREBASE_APP_ID: string;
+  export const FIREBASE_MEASUREMENT_ID: string;
 }
